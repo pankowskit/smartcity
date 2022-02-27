33a7ba090063d376cb4538eb6722b1a1ce63ae1d
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 15:26:55 +0200

    routing

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,10 @@
+import About from "./views/about";
 import Comingsoon from "./views/comingsoon";
+import Contact from "./views/contact";
 import Error from "./views/error";
+import Home from "./views/home";
 import Privacy from "./views/privacy";
+import Services from "./views/services";
 
 const ERRORS = {
     "401":{
@@ -18,7 +22,11 @@ const ERRORS = {
 }
 
 const PATHS = [
-    {menuItem:false,menuName:"",path:"/",component:<Comingsoon/>},
+    {menuItem:true,menuName:"Home",path:"/",component:<Home/>},
+    {menuItem:true,menuName:"Services",path:"/services",component:<Services/>},
+    {menuItem:true,menuName:"About",path:"/about",component:<About/>},
+    {menuItem:true,menuName:"Contact",path:"/contact",component:<Contact/>},
+    {menuItem:false,menuName:"",path:"/comingsoon",component:<Comingsoon/>},
     {menuItem:false,menuName:"",path:"/privacy",component:<Privacy/>},
     {menuItem:false,menuName:"",path:"*",component:<Error errorData={ERRORS['404']}/>}
 ];
